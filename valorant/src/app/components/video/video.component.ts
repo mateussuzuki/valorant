48b142caf526f2404653a844910404c6e789129b
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-12-06 20:16:26 -0300

    arrumando o "carrossel" dos gifs

--- valorant/src/app/components/video/video.component.ts
+++ valorant/src/app/components/video/video.component.ts
@@ -7,7 +7,7 @@ import { imageGifs } from './gifs.mock';
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit{
 
   gifs:ImageGifs[] = imageGifs
   pass:number = 0
@@ -17,7 +17,20 @@ export class VideoComponent {
     this.passGifs = this.gifs[this.pass]
   }
 
+  ngOnInit(): void {
+    this.playNextGif()
+  }
 
+  playNextGif():void {
+    setTimeout(() => {
+      this.passGifs = this.gifs[this.pass]
+      this.pass++
+      if(this.pass == 3) {
+        this.pass = 0
+      }
+      this.playNextGif()
+    }, 6000)
+  }
 
   }
 
