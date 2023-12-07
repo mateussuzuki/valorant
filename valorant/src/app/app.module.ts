c0115d9a553c901ccaef14c5df0c5e1073fdf306
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-12-06 21:25:45 -0300

    arrumando responsividade

--- valorant/src/app/app.module.ts
+++ valorant/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { VideoComponent } from './components/video/video.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    VideoComponent
+    VideoComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
--- valorant/src/app/components/video/video.component.ts
+++ valorant/src/app/components/video/video.component.ts
@@ -29,7 +29,7 @@ export class VideoComponent implements OnInit{
         this.pass = 0
       }
       this.playNextGif()
-    }, 6000)
+    }, 5500)
   }
 
   }
