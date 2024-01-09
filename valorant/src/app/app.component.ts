dfff69ed10127a18c79562581773998ce23ae706
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-01-09 12:13:19 -0300

    animação scroll

--- valorant/src/app/app.component.ts
+++ valorant/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'valorant';
+
+  ngOnInit() {
+    AOS.init()
+    window.addEventListener('load',AOS.refresh)
+  }
 }
