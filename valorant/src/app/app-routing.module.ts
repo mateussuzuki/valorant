dc9cff5114d2ad9205f29181ac7da8f4ab807737
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-01-16 11:47:31 -0300

    rota link agentes

--- valorant/src/app/app-routing.module.ts
+++ valorant/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { AgentsInfoScreenComponent } from './components/agents-info-screen/agents-info-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainScreenComponent },
+  { path: 'agents', component: AgentsInfoScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
