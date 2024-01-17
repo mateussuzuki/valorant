c1d80e394478b4c1772a110eb8a0f720806ccb3e
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-01-17 17:37:21 -0300

    transição na troca de agentes. pagina agentes

--- valorant/src/app/components/agents-info-screen/agents-info-screen.component.ts
+++ valorant/src/app/components/agents-info-screen/agents-info-screen.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Role } from 'src/app/models/agents.model';
 import { AgentsInfo } from 'src/app/models/agents.model';
 import { ApiService } from 'src/app/services/api.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-agents-info-screen',
   templateUrl: './agents-info-screen.component.html',
-  styleUrls: ['./agents-info-screen.component.scss']
+  styleUrls: ['./agents-info-screen.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      state('out', style({opacity: 0})),
+      
+      transition("in<=>out", animate('0.2s'))
+    ])
+  ]
 })
 
 export class AgentsInfoScreenComponent implements OnInit {
@@ -14,6 +22,7 @@ export class AgentsInfoScreenComponent implements OnInit {
   agentAllInfo:AgentsInfo[] = []
   selectedInfo!:AgentsInfo
 
+  imgFade:string = "in"
   selectedIcon!:AgentsInfo
 
   constructor (private agentsService: ApiService) {
@@ -36,7 +45,8 @@ export class AgentsInfoScreenComponent implements OnInit {
     })
   }
 
-  selectInfo(info:AgentsInfo): void {
+  async selectInfo(info:AgentsInfo) {
+    await this.delay(200)
     this.selectedInfo = info
   }
 
@@ -44,4 +54,13 @@ export class AgentsInfoScreenComponent implements OnInit {
     this.selectedIcon = icon
   }
 
+  async switchAnimation() {
+    this.imgFade = "out"
+    await this.delay(200)
+    this.imgFade = "in"
+  }
+
+  delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 }
